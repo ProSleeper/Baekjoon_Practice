b052c6f1c791bf0754250952f850448febadda2e
ProSleeper <ingngnin@gmail.com>
2021-03-20 05:34:53 +0900

    Baekjoon 1806번 부분합 풀이

--- Number_Sum2.cpp
+++ Number_Sum2.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-
-int main()
-{
-	int numberN = 0;
-	int numberM = 0;
-	vector<int> numberSequence;
-
-	cin >> numberN >> numberM;
-
-	int inputNumber = 0;
-	for(int i = 0; i < numberN; i++){
-		cin >> inputNumber;
-		numberSequence.push_back(inputNumber);
-	}
-
-	int front = 0;
-	int end = 0;
-	int sumValue = 0;
-	int answerCount = 0;
-
-	while(end < numberN){
-		for(int i = front; i <= end; i++){
-			sumValue += numberSequence[i];
-		}
-
-		if(sumValue < numberM){
-			end++;
-		}
-		else if(sumValue > numberM){
-			front++;
-		}
-		else{
-			answerCount++;
-			end++;
-		}
-		sumValue = 0;
-	}
-
-
-	cout << answerCount;
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int numberN = 0;
+//	int numberM = 0;
+//	vector<int> numberSequence;
+//
+//	cin >> numberN >> numberM;
+//
+//	int inputNumber = 0;
+//	for(int i = 0; i < numberN; i++){
+//		cin >> inputNumber;
+//		numberSequence.push_back(inputNumber);
+//	}
+//
+//	int front = 0;
+//	int end = 0;
+//	int sumValue = 0;
+//	int answerCount = 0;
+//
+//	while(end < numberN){
+//		for(int i = front; i <= end; i++){
+//			sumValue += numberSequence[i];
+//		}
+//
+//		if(sumValue < numberM){
+//			end++;
+//		}
+//		else if(sumValue > numberM){
+//			front++;
+//		}
+//		else{
+//			answerCount++;
+//			end++;
+//		}
+//		sumValue = 0;
+//	}
+//
+//
+//	cout << answerCount;
+//
+//	return 0;
+//}
